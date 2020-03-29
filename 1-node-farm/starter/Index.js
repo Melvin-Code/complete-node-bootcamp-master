bd091f7cd28e85361a2dd74755ed8998e0c404cc
melvin-Code <melvingonzalez470@gmail.com>
2020-03-29 19:26:30 -0400

    api completed

---
+++ 1-node-farm/starter/Index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+// blocking syncronus way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+// console.log(textIn)
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut)
+
+// console.log('file written')
+
+// non-blocking
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+
+//             })
+//         });
+//     });
+// });
+// console.log('will read file')
+//////////////////////////////
+
+////server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);  
+  
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("this is the product");
+  } else if (pathName === "/api") {
+
+     res.writeHead(200, {'Content-type' : 'application/json'})
+     res.end(data);
+   
+
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html"
+    });
+    res.end("<h1>this page can no be found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to request on port 8000");
+});
